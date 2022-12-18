3772987dd6258f33ecaae29e88d172228d38f590
Aimony <2249398422@qq.com>
2022-12-18 17:13:54 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -14,7 +14,8 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
 
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: "http://124.71.9.121:8080",
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000, // request timeout
 
